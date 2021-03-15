77d0ce909bc9b4329365b6eb69f4647cb5fce36f
user <user@DESKTOP-JUOC629>
2021-03-15 15:59:23 +0530

    homepage code

---
+++ jenkirelated/src/test/java/com/issrac/creacontacttest/hometest.java
@@ -0,0 +1,11 @@
+package com.issrac.creacontacttest;
+
+import org.testng.annotations.Test;
+
+public class hometest {
+	@Test
+	public void opportunity(String name,String date,int no){
+		
+	}
+
+}
