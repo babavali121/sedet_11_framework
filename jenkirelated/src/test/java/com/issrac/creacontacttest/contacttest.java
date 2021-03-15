081d45df65b567aa1e9928b580c6c439898d1f70
user <user@DESKTOP-JUOC629>
2021-03-15 13:36:50 +0530

    createcontactcode

---
+++ jenkirelated/src/test/java/com/issrac/creacontacttest/contacttest.java
@@ -0,0 +1,20 @@
+package com.issrac.creacontacttest;
+
+import org.testng.annotations.Test;
+
+public class contacttest {
+	@Test
+	public void createcontacttest(){
+		System.out.println("execute create contact");
+	}
+	@Test
+	public void modifycreatecontacttest(){
+		System.out.println("execute create contact");
+	}
+	@Test
+	public void deletecreatecontacttest(){
+		System.out.println("execute create contact");
+	}
+	
+
+}
