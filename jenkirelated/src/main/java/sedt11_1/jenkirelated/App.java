9a7a2a1deaa7b91777c7b95cd5b7ed721a165fc2
user <user@DESKTOP-JUOC629>
2021-03-15 12:58:19 +0530

    commit fistproject

---
+++ jenkirelated/src/main/java/sedt11_1/jenkirelated/App.java
@@ -0,0 +1,13 @@
+package sedt11_1.jenkirelated;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
